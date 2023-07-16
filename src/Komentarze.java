57b69a8fdc271f2442aba47216dacadc1dd0241d
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-07-16 11:21:28 +0200

    Komentarze

---
+++ src/Komentarze.java
@@ -0,0 +1,11 @@
+public class Komentarze
+{
+    public static void main(String[] args)
+    {
+        // Komentarz
+        System.out.println("Wypisz coś na ekran");
+        /*
+        Dowolny komentarz
+         */
+    }
+}
