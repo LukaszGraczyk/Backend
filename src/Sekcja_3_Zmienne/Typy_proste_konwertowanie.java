a883fec13b29a59b16a6ef5cd37f74a856143679
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-07-21 22:01:14 +0200

    Converting part 2

--- src/Sekcja_3_Zmienne/Typy_proste_konwertowanie.java
+++ src/Sekcja_3_Zmienne/Typy_proste_konwertowanie.java
@@ -28,14 +28,23 @@ public class Typy_proste_konwertowanie
         // Konwertowanie jawne (rzutowanie)
 
         long zmiennaLong = 123;
-        int zmiennaInt = (int) zmiennaLong; // Konwersja odwrotna do "rozszerzającej" jawna
+        int zmiennaInt = (int) zmiennaLong; // Konwersja odwrotna do "rozszerzającej" jawna (rzutowanie)
 
+        int intLiczba = 6;
+        double doubleLiczba = 2.2;
 
+        int wynik = intLiczba / (int) doubleLiczba;
+        System.out.println("Wynik :" + wynik);
 
+        double doubleLiczba2 = 1.2;
+        double doubleLiczba3 = 12.4;
 
-        
+        int wynik2 = (int) (doubleLiczba3 / doubleLiczba2); // Rzutowanie wyniku dzielenia zmiennych double
+        System.out.println("Wynik :" + wynik2);
 
-
-
+        char c ='A';
+        int e = c;
+        System.out.println("Konwersja char'a na int'a: " + e);
+        // Konwersja ta daje w rezultacie kod ASCII
     }
 }
