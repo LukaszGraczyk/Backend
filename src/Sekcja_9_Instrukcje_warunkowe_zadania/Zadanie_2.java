d20135a2a600359f643e0adb2e86a86e74e7aa6d
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-07-26 13:35:18 +0200

    Exercise 2 - if statement

--- src/Sekcja_9_Instrukcje_warunkowe_zadania/Zadanie_2.java
+++ src/Sekcja_9_Instrukcje_warunkowe_zadania/Zadanie_2.java
@@ -19,7 +19,23 @@ public class Zadanie_2
         Uwaga: zrób dodatkowe zabezpieczenie, jeżeli ktoś miałby więcej
          */
 
+        double zarobki = 102999;
 
-
+        if(zarobki<0)
+        {
+            System.out.println("Nie trzeba płacić podatku");
+        }
+        else if(zarobki<=85528)
+        {
+            double podatek = zarobki * 0.17;
+            System.out.println("Podatek do zapłaty wynosi: " + podatek);
+        }
+        else
+        {
+            double podatekPonizejProgu = 85528 * 0.17;
+            double podatekPowyzejProgu = (zarobki-85528) * 0.32;
+            double podatek = podatekPonizejProgu + podatekPowyzejProgu;
+            System.out.println("Podatek do zapłaty wynosi: " + podatek);
+        }
     }
 }
