bfc4ec6a79c4ce0972439f404061601b716029e9
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-07-26 13:14:01 +0200

    Exercise 1 - if statement

---
+++ src/Sekcja_9_Instrukcje_warunkowe_zadania/Zadanie_2.java
@@ -0,0 +1,25 @@
+package Sekcja_9_Instrukcje_warunkowe_zadania;
+
+public class Zadanie_2
+{
+    public static void main(String[] args)
+    {
+        /*
+        Twoim zadaniem jest obliczenie podatku od dochodu. Obecnie wynosi on 17%. Wiemy, także doświadczeni programiści
+        potrafią zarabiać pokaźne sumy, a od pewnego dochodu zwiększony jest podatek do 32%. Sprawdź ile podatku
+        musiałby zapłacić początkujący programista, zanim przekroczy próg wynoszący 85 528 zł, a ile już ten doświadczony.
+
+        Wzór:
+        Dla dochodu do 85 528 zł podatek wynosi 17%,
+        dla dochodu powyżej 85 528 zł liczy się go następująco:
+        - obliczamy podatek 17% z 85 528,
+        - dodatkowo obliczamy podatek 32% od nadwyżki ponad 85 528 zł,
+        czyli dochód pomniejszony o 85 528 zł i z tego 32%.
+        (oczywiście obliczamy orientacyjnie, gdyż nie bierzemy pod uwagę np. ulgi podatkowej).
+        Uwaga: zrób dodatkowe zabezpieczenie, jeżeli ktoś miałby więcej
+         */
+
+
+
+    }
+}
