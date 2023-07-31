af4463936921b91c232d71d5d9ff3d31658955b0
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-07-31 23:53:29 +0200

    String

---
+++ src/Sekcja_13_String.java
@@ -0,0 +1,8 @@
+public class Sekcja_13_String
+{
+    public static void main(String[] args)
+    {
+        String nazwaDlaStringa = "Nazwa dla Stringa";
+        System.out.println(nazwaDlaStringa);
+    }
+}
