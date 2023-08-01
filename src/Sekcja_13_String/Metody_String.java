0644d982fad4482a5fa64a14af1ced2fdf0c341f
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-08-01 23:11:16 +0200

    String_methods

--- src/Sekcja_13_String/Metody_String.java
+++ src/Sekcja_13_String/Metody_String.java
@@ -1,4 +1,14 @@
 package Sekcja_13_String;
 
-public class Metody_String {
+public class Metody_String
+{
+    public static void main(String[] args)
+    {
+        String tekst = "Jakiś tekst";
+        System.out.println("Length: " + tekst.length());
+        System.out.println("isEmpty: " + tekst.isEmpty());
+
+        char pojedynczyZnak = tekst.charAt(6);
+        System.out.println("charAt: " + pojedynczyZnak);
+    }
 }
