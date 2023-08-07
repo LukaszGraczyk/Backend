1ea95ec365d0da313b0062f6050d052c5ca3e662
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-08-07 19:05:37 +0200

    Null

--- src/Sekcja_15_Klasy_kontynuacja/MainPolaStaticFinal.java
+++ src/Sekcja_15_Klasy_kontynuacja/MainPolaStaticFinal.java
@@ -6,5 +6,14 @@ public class MainPolaStaticFinal
     {
         KlasaStaticFinal obiekt = new KlasaStaticFinal();
         obiekt.pole = 10;
+
+        KlasaStaticFinal.poleStatic = 15; // Dla pola static nie trzeba odwoływać się do niego poprzez obiekt.
+        obiekt.poleStatic = 123;
+
+        System.out.println("KlasaStaticFinal.poleStatic: " + KlasaStaticFinal.poleStatic);
+
+        KlasaStaticFinal.metodaStatyczna(); // Metoda statyczna
+
+        System.out.println(KlasaStaticFinal.JAKIS_TEKST); // Wypisanie pola string statycznego będącego również final
     }
 }
