6e09bf527142c0b0c7fca6fa08817295fb1bee3b
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-08-23 13:42:36 +0200

    Interface

---
+++ src/Sekcja_21_Klasy_abstrakcyjne_interfejsy_i_inne/Interfejs/PrzykladowyInterface.java
@@ -0,0 +1,7 @@
+package Sekcja_21_Klasy_abstrakcyjne_interfejsy_i_inne.Interfejs;
+
+public interface PrzykladowyInterface
+{
+    public void metodaWInterface();
+    public int metodaZParametremWInterfejsie(String z);
+}
