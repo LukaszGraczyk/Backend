d50e54698ecd2be710c6b2b8053f3996f8e7b825
Łukasz Graczyk <graczyklukasz88@gmail.com>
2023-09-03 20:26:57 +0200

    File loading

---
+++ src/Sekcja_29_IO_File_zapisywanie_wczytywanie_pliku/Wczytywanie_pliku/KlasaLokalizacjiPlikow.java
@@ -0,0 +1,6 @@
+package Sekcja_29_IO_File_zapisywanie_wczytywanie_pliku.Wczytywanie_pliku;
+
+public class KlasaLokalizacjiPlikow
+{
+    public static String lokalizacja1 = "C:/Dokumenty/Dane/Informatyka/Kursy programowania/JavaProgrammingCourse/src/Sekcja_29_IO_File_zapisywanie_wczytywanie_pliku/Wczytywanie_pliku/Plik_testowy_nr_1.txt";
+}
